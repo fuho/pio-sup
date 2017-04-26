c76fde6d709fc3bb659e0c4c24176be73a828256
Ondrej <odolejsi@icloud.com>
2017-04-25 22:21:29 -0400

    Servo goes all like LOOK AT ME! I CAN DO THINGS

--- examples/servo/src/main.cpp
+++ examples/servo/src/main.cpp
@@ -1,17 +1,20 @@
-#include <arduino.h>			// All sorts of goodies here...
+#include <arduino.h>	// All sorts of goodies here...
+#include "Servo.h"		// Servo library
 
-const uint8_t LED = 2; 		// LED is on pin 2
+const uint8_t SERVO = D0; // Servo is on pin D0
+Servo servo;
 
 void setup(){							// This is run once and only once at the boot
-	pinMode(LED, OUTPUT); 	// We intend to write to this pin
+	pinMode(SERVO, OUTPUT); // We intend to write to this pin
+	servo.attach(SERVO);    // Tell the Servo library where it is attached
 	Serial.begin(115200);		// Initialize serial port
 }
 
-void loop(){							// All your programs will have an execution loop
-	digitalWrite(LED, LOW);	// This might look counter-intuitive but the LED ..
-	Serial.println("ON");		// .. anode is connected to 3.3V and the cathode is..
-	delay(1000);						// .. connected to PIN 2. When we then pull the pin ..
-	digitalWrite(LED, HIGH);// .. low (0V), we create a voltage differential and ..
-	Serial.println("OFF");	// .. the current flows. When we pull it high (3.3V) ..
-	delay(1000);						// .. both LED pins are at 3.3V hence no current flow.
+void loop(){
+	Serial.println("Servo 0");
+	servo.write(0);		// Zero degrees
+  delay(1000);			// Wait a sec..
+	Serial.println("Servo 179");
+	servo.write(179); // 179 degrees
+  delay(1000); 			// Wait a sec..
 }
